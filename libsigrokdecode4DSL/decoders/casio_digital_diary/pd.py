bd4f15188201e6cbd3a2a2cb530c3421a807f432
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2021-09-05 18:06:05 +0100

    Break type & address

--- libsigrokdecode4DSL/decoders/casio_digital_diary/pd.py
+++ libsigrokdecode4DSL/decoders/casio_digital_diary/pd.py
@@ -12,57 +12,51 @@ def _get_annotation_index(annotations, name):
 
 @dataclass
 class Frame:
-    length: int
-    frame_type: List[int]
+    length: int  # Max: 0x80
+    type: int
+    address: int
     data: List[int]
     checksum: int
     startsample: int
     endsample: int
 
-    _text_types = {
-        0x0: "Name",
-    }
-
     @classmethod
     def new_empty(cls):
-        return cls(0, [], [], 0, 0, 0)
+        return cls(0, 0, 0, [], 0, 0, 0)
 
     def get_type_str(self) -> str:
-        if self.length == 0x2 and self.frame_type == [0x0, 0x0, 0x2]:
+        if self.length == 0x2 and self.type == 0x0 and self.address == 0x200:
             return "segment-start"
         if (
             self.length == 0x1
-            and self.frame_type == [0x71, 0x0, 0x0]
+            and self.type == 0x71
+            and self.address == 0x0
             and self.data[0] in [0x1, 0x2, 0x4]
         ):
             return "color"
-        if self.length == 0xA and self.frame_type == [0xF0, 0x0, 0x0]:
+        if self.length == 0xA and self.type == 0xF0 and self.address == 0x0:
             return "date"
-        if self.length == 0xA and self.frame_type == [0xF4, 0x0, 0x0]:
+        if self.length == 0xA and self.type == 0xF4 and self.address == 0x0:
             return "todo-date"
-        if self.length == 0x1 and self.frame_type == [0x72, 0x0, 0x0]:
+        if self.length == 0x1 and self.type == 0x72 and self.address == 0x0:
             return "todo-priority"
-        if self.length == 0x5 and self.frame_type == [0xE4, 0x0, 0x0]:
+        if self.length == 0x5 and self.type == 0xE4 and self.address == 0x0:
             return "todo-time"
-        if self.length == 0x5 and self.frame_type == [0xC4, 0x0, 0x0]:
+        if self.length == 0x5 and self.type == 0xC4 and self.address == 0x0:
             return "todo-alarm"
-        if self.length == 0x20 and self.frame_type == [0x78, 0x0, 0x0]:
+        if self.length == 0x20 and self.type == 0x78 and self.address == 0x0:
             return "calendar-date-color-highlight"
-        if self.length in [0xB, 0x5] and self.frame_type == [0xE0, 0x0, 0x0]:
+        if self.length in [0xB, 0x5] and self.type == 0xE0 and self.address == 0x0:
             return "time"
-        if self.length == 0x5 and self.frame_type == [0xC0, 0x0, 0x0]:
+        if self.length == 0x5 and self.type == 0xC0 and self.address == 0x0:
             return "alarm"
-        if self.length == 0x1 and self.frame_type == [0x21, 0x0, 0x0]:
+        if self.length == 0x1 and self.type == 0x21 and self.address == 0x0:
             return "illustration"
-        if (
-            self.frame_type[0] == 0x80
-            and self.frame_type[1] in self._text_types.keys()
-            and self.frame_type[2] == 0x0
-        ):
+        if self.type == 0x80:
             return "text"
-        if self.length == 0x0 and self.frame_type == [0x0, 0x0, 0x1]:
+        if self.length == 0x0 and self.type == 0x0 and self.address == 0x100:
             return "record-end"
-        if self.length == 0 and self.frame_type == [0x0, 0x0, 0xFF]:
+        if self.length == 0x0 and self.type == 0x0 and self.address == 0xFF00:
             return "finish"
         return "unknown"
 
@@ -141,8 +135,7 @@ class Frame:
             return f"Illustration: {self.data[0]}"
 
         if type_str == "text":
-            text_type = self._text_types.get(self.frame_type[1], "Unknown")
-            return f"{text_type}: " + "".join(
+            return f"Text ({self.address}): " + "".join(
                 chr(d) if chr(d).isprintable() else f"({hex(d)})" for d in self.data
             )
 
@@ -383,23 +376,46 @@ class Decoder(srd.Decoder):
     def _decode_sender_frame_header_type(self, startsample, endsample, data):
         value = self._decode_hex(data)
         if value is not None:
-            if len(self._frame.frame_type) < 3:
-                self._frame.frame_type.append(value)
+            self._frame.type = value
+            self.put(
+                self._chunk_startsample,
+                endsample,
+                self.out_ann,
+                [
+                    _get_annotation_index(self.annotations, "frame-header"),
+                    ["Type: " + hex(self._frame.type)],
+                ],
+            )
+            self._data_count = self._frame.length
+            self._sender_decode_function = self._decode_sender_frame_header_address
+        else:
+            self._chunk_startsample = startsample
+        return True
 
-            if len(self._frame.frame_type) == 3:
+    def _decode_sender_frame_header_address(self, startsample, endsample, data):
+        value = self._decode_hex(data)
+        if value is not None:
+            if len(self._address_bytes) < 2:
+                self._address_bytes.append(value)
+
+            if len(self._address_bytes) == 2:
+                self._frame.address = (self._address_bytes[1] << 8) | (
+                    self._address_bytes[0] & 0xFF
+                )
                 self.put(
                     self._chunk_startsample,
                     endsample,
                     self.out_ann,
                     [
                         _get_annotation_index(self.annotations, "frame-header"),
-                        ["Type: " + " ".join(hex(v) for v in self._frame.frame_type)],
+                        ["Address: " + hex(self._frame.address)],
                     ],
                 )
+                self._address_bytes = []
                 self._data_count = self._frame.length
                 self._sender_decode_function = self._decode_sender_frame_data
         else:
-            if not len(self._frame.frame_type):
+            if not len(self._address_bytes):
                 self._chunk_startsample = startsample
         return True
 
@@ -512,8 +528,7 @@ class Decoder(srd.Decoder):
         """
         self._sender_decode_function = self._decode_sender_sync_or_frame
         self._frame = Frame.new_empty()
-        if hasattr(self, "_frame_address_high"):
-            delattr(self, "_frame_address_high")
+        self._address_bytes = []
 
     def decode(self, startsample, endsample, data):
         """
