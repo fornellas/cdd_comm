bc155b8c42261710e3c762eae0df191d191dde3f
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2021-10-05 23:36:01 +0100

    Extract flow control class

--- cdd_comm/sender.py
+++ cdd_comm/sender.py
@@ -1,5 +1,6 @@
 import time
-from typing import Dict, List, Optional
+from types import TracebackType
+from typing import Dict, List, Literal, Optional, Type
 
 import serial
 
@@ -7,102 +8,116 @@ from . import frame as frame_mod
 from . import record as record_mod
 
 
-class Sender:
-    SYNC_1: int = 0x0D
-    SYNC_2: int = 0x0A
-    XON: int = 0x11
-    XOFF: int = 0x13
-    ACK: int = 0x23
-    NACK: int = 0x3F
-
+class FlowControlSerial:
     def __init__(
         self,
-        directory_data: Dict[frame_mod.Directory, List[record_mod.Record]],
         port: str,
         baudrate: int,
         bytesize: int,
         parity: str,
-    ):
-        self.directory_data = directory_data
+    ) -> None:
         self.port = port
         self.baudrate = baudrate
         self.bytesize = bytesize
         self.parity = parity
-
-    def _get_serial(self) -> serial.Serial:
-        print("Opening serial port")
-        return serial.Serial(
+        self._serial = serial.Serial(
             port=self.port,
             baudrate=self.baudrate,
             bytesize=self.bytesize,
             parity=self.parity,
         )
+        self._read_buff: List[int] = []
 
-    def _wait_xon(self, ser: serial.Serial) -> None:
-        while True:
-            read = ser.read(size=1)[0]
-            if read == self.XOFF:
-                print("< XOFF")
-            elif read == self.XON:
-                print("< XON")
-                return
+    def __enter__(self) -> "FlowControlSerial":
+        if not self._serial.isOpen:
+            self._serial.open()
+        self._read_buff = []
+        return self
 
-    def _check_wait_xoff(self, ser: serial.Serial) -> Optional[int]:
-        if ser.in_waiting:
-            read = ser.read(size=1)[0]
-            if read == self.XOFF:
-                print("< XOFF")
-                self._wait_xon(ser)
-                return None
-            return read
-        return None
+    def __exit__(
+        self,
+        exctype: Optional[Type[BaseException]],
+        excinst: Optional[BaseException],
+        exctb: Optional[TracebackType],
+    ) -> Literal[False]:
+        self._serial.close()
+        return False
 
-    def _receive_byte(self, ser: serial.Serial) -> int:
-        while True:
-            read = ser.read(size=1)[0]
-            if read == self.XOFF:
-                self._wait_xon(ser)
-            return read
-
-    def _send_bytes(self, ser: serial.Serial, data: bytes) -> None:
-        for d in data:
-            read = self._check_wait_xoff(ser)
-            if read != None:
-                if read == self.NACK:
-                    raise RuntimeError("NACK")
+    def write(self, data: bytes) -> None:
+        for b in data:
+            xoff: bool = False
+            while True:
+                if self._serial.in_waiting or xoff:
+                    read = self._serial.read(size=1)[0]
+                    if read == serial.XOFF[0]:
+                        print("< XOFF")
+                        xoff = True
+                    elif read == serial.XON[0]:
+                        print("< XON")
+                        break
+                    else:
+                        self._read_buff.append(read)
                 else:
-                    raise RuntimeError(f"Unexpected: {repr(read)}")
-            if ser.write(bytes([d])) != 1:
+                    break
+            if self._serial.write([b]) != 1:
                 raise RuntimeError("Short write!")
-            # print(f"    > {hex(d)}")
             # This seem to be required otherwise we get constant XOFF
             time.sleep(0.0011)
 
-    def _sync(self, ser: serial.Serial) -> None:
-        while True:
-            print("> Sync 1")
-            ser.write(bytes([self.SYNC_1]))
-            time.sleep(0.01)
-            print("> Sync 2")
-            ser.write(bytes([self.SYNC_2]))
-            original_timeout = ser.timeout
-            ser.timeout = 0.2  # FIXME
-            xon = ser.read(size=1)
-            ser.timeout = original_timeout
-            if len(xon):
-                print("< XON")
-                if xon[0] == self.XON:
-                    break
+    def read(self) -> int:
+        if self._read_buff:
+            return self._read_buff.pop(0)
+        else:
+            return self._serial.read(size=1)[0]
+
+    def wait_for_xon(self, timeout: float) -> bool:
+        original_timeout = self._serial.timeout
+        self._serial.timeout = timeout
+        try:
+            data = self._serial.read(size=1)
+            if len(data):
+                if data[0] == serial.XON[0]:
+                    print("< XON")
+                    return True
                 else:
-                    print(f"< Unexpected: {hex(xon)[0]}")
-            print("! Timeout")
+                    raise ValueError(f"Unexpected: {data[0]}")
+            return False
+        finally:
+            self._serial.timeout = original_timeout
 
-    def _send_frame(self, ser: serial.Serial, frame: frame_mod.Frame) -> None:
+
+class Sender:
+    ACK: int = 0x23
+    NACK: int = 0x3F
+
+    def __init__(
+        self,
+        port: str,
+        baudrate: int,
+        bytesize: int,
+        parity: str,
+    ) -> None:
+        self.port = port
+        self.baudrate = baudrate
+        self.bytesize = bytesize
+        self.parity = parity
+
+    def _sync(self, ser: FlowControlSerial) -> None:
+        while True:
+            print("> CR")
+            ser.write(serial.CR)
+            time.sleep(0.01)
+            print("> LF")
+            ser.write(serial.LF)
+            if ser.wait_for_xon(0.2):
+                break
+
+    def _send_frame(self, ser: FlowControlSerial, frame: frame_mod.Frame) -> None:
         # print(f"  > Frame: {frame}")
-        self._send_bytes(ser, frame.bytes())
+        ser.write(frame.bytes())
 
-    def _wait_for_ack(self, ser: serial.Serial) -> None:
-        read = self._receive_byte(ser)
+    def _wait_for_ack(self, ser: FlowControlSerial) -> None:
+        read = ser.read()
         if read == self.ACK:
             print("< ACK")
             return
@@ -113,23 +128,31 @@ class Sender:
             raise RuntimeError(f"Unexpected data: {hex(read)}")
 
     def _send_directory(
-        self, ser: serial.Serial, directory: frame_mod.Directory
+        self, ser: FlowControlSerial, directory: frame_mod.Directory
     ) -> None:
         print(f"> {directory}")
         self._send_frame(ser, directory)
         self._wait_for_ack(ser)
 
-    def _send_record(self, ser: serial.Serial, record: record_mod.Record) -> None:
+    def _send_record(self, ser: FlowControlSerial, record: record_mod.Record) -> None:
         print(f"> {record}")
         for frame in record.to_frames():
             self._send_frame(ser, frame)
         self._send_frame(ser, frame_mod.EndOfRecord.get())
         self._wait_for_ack(ser)
 
-    def send_directory_data(self) -> None:
-        with self._get_serial() as ser:
+    def send_directory_data(
+        self,
+        data: Dict[frame_mod.Directory, List[record_mod.Record]],
+    ) -> None:
+        with FlowControlSerial(
+            port=self.port,
+            baudrate=self.baudrate,
+            bytesize=self.bytesize,
+            parity=self.parity,
+        ) as ser:
             self._sync(ser)
-            for directory, records in self.directory_data.items():
+            for directory, records in data.items():
                 self._send_directory(ser, directory)
                 for record in records:
                     self._send_record(ser, record)
@@ -138,14 +161,19 @@ class Sender:
 
 if __name__ == "__main__":
     Sender(
-        directory_data={
+        port="/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0",
+        baudrate=9600,
+        bytesize=serial.SEVENBITS,
+        parity=serial.PARITY_NONE,
+    ).send_directory_data(
+        {
             frame_mod.TelephoneDirectory.get(): [
                 record_mod.Telephone(
                     color=frame_mod.Colors.BLUE,
-                    name="John Doe Blue",
-                    number="123-456",
-                    address="Nowhere St",
-                    field1="Average guy",
+                    name="01234567890" * 10 * 3,
+                    number="01234567890" * 10 * 3,
+                    address="01234567890" * 10 * 3,
+                    field1="01234567890" * 10 * 3,
                     field2=None,
                     field3=None,
                     field4=None,
@@ -177,9 +205,5 @@ if __name__ == "__main__":
                     field6=None,
                 ),
             ],
-        },
-        port="/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0",
-        baudrate=9600,
-        bytesize=serial.SEVENBITS,
-        parity=serial.PARITY_NONE,
-    ).send_directory_data()
+        }
+    )
